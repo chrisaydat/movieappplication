08f1457790cdf975ecfc25508c35bd2a2560feaa
chrisaydat <chrisaydat@gmail.com>
2023-09-08 06:12:44 +0000

    app initialisation

---
+++ android/app/src/main/kotlin/com/example/movieappplication/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.movieappplication
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+      home: CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          middle: Text('MovieApp'),
+        ),
+        child: Center(),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
